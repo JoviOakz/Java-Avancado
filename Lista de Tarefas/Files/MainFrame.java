268855a5761ca2bcd40e62664c14883a1c5e6280
JoviOakz <joao.mateussdc@gmail.com>
2024-05-14 15:13:33 -0300

    feat: added Lista de Tarefas

---
+++ Lista de Tarefas/Files/MainFrame.java
@@ -0,0 +1,12 @@
+package Files;
+
+// import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    // private ArrayList<String> tasks = new ArrayList<>();
+
+    public MainFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
