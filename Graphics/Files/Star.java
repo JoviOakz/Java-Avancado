7ecdd2d28f0f53d8e1eaac67b4924ed9d66ac125
JoviOakz <joao.mateussdc@gmail.com>
2024-05-16 13:20:38 -0300

    test

--- Graphics/Files/Star.java
+++ Graphics/Files/Star.java
@@ -2,6 +2,8 @@ package Files;
 
 import javax.swing.JComponent;
 
-public class Star extends JComponent{
-    
+public class Star extends JComponent {
+    JComponent abo = new JComponent() {
+        
+    };
 }
